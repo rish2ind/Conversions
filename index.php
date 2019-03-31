9525ec91169ba23669e204164a92c136c05a1133
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-03-31 22:08:23 +0530

    Home page

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Conversions</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<div class="select">
+	<h1>Choose the following</h1>
+
+	<ul id="measurement_types">
+	<li><a href="Length.php">Converting Lengths</a></li>
+	<li><a href="#">Converting Volume and weight</a></li>
+	<li><a href="Area.php">Converting Area</a></li>
+	<li><a href="#">Converting Speed</a></li>
+	</ul>
+	</div>
+	</body>
+</html>
